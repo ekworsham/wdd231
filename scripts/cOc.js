14abdbbcab7081ff8503715fef1ac62c19929a53
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-09-21 21:38:10 -0400

    update

---
+++ scripts/cOc.js
@@ -0,0 +1,10 @@
+let currentYear = document.querySelector("#currentYear");
+let lastModified = document.querySelector("#lastModified");
+
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
