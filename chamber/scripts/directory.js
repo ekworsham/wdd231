648b8a0bc3fcffda64d7e318ebe321262953fe65
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-09-30 22:04:52 -0400

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -13,3 +13,36 @@ hamburgerElement.addEventListener('click', () => {
     hamburgerElement.classList.toggle('open');
 })
 
+const url="https://ekworsham.github.io/wdd231/chamber/data/members.json";
+
+const cards=document.querySelector("#card");
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    createMemberCards(data);
+}
+
+function createMemberCards(data) {
+
+    data.forEach(member => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "businessCard1");
+        card.innerHTML = `
+            <div class="cardHeader1">
+                <img src="${member.image}" alt="${member.name}" class="profileImg1">
+                <div class="contactInfo1">
+                    <h3>${member.name}<br>${member.title}</h3>
+                    <p>ADDRESS: ${member.address}</p>
+                    <p>PHONE: ${member.phoneNumber}</p>
+                    <p>URL: ${member.url}</p>
+                    <p>MEMBER LEVEL: ${member.membershipLevel}</p>
+                </div>
+            </div>
+        `;
+        cards.appendChild(card);
+    });
+}
+
+getMemberData();
