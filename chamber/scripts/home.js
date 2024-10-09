268a0a1622553740ebc945f65501f9c7c6f3af89
ekworsham <145511698+ekworsham@users.noreply.github.com>
2024-10-08 22:02:31 -0400

    update

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -105,3 +105,41 @@ function displayForecastResults(dataF) {
     
 }
 fetchForecast();
+
+// ************************
+// get member cards
+// ************************
+const memberUrl = "https://github.com/ekworsham/wdd231/blob/main/chamber/data/home.json";
+
+const cards=document.querySelector("#card");
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+
+async function createMemberCards() {
+    cards.innerHTML = " ";
+    const data = await getMemberData();
+    data.forEach(member => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "businessCard1");
+        card.innerHTML = `
+            <div class="cardHeader1">
+                <img src="${member.image}" alt="${member.name}" class="profileImg1">
+                <div class="contactInfo1">
+                    <h3>${member.name}<br>${member.title}</h3>
+                    <p>ADDRESS: ${member.address}</p>
+                    <p>PHONE: ${member.phoneNumber}</p>
+                    <p>URL: ${member.url}</p>
+                    <p>MEMBER LEVEL: ${member.membershipLevel}</p>
+                </div>
+            </div>
+        `;
+        cards.appendChild(card);
+    });
+}
+
+createMemberCards();
